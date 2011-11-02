5a166c3b783045037c47dcd920494b0c85927801
Andrew Dollard <andrewdollard@gmail.com>
2011-11-02 11:50:14 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'http://rubygems.org'
+
+# This is a comment
+
+gem 'rails', '3.1.1'
+gem 'gravatar_image_tag'
+gem 'will_paginate'
+gem 'rack', '!= 1.3.4' 
+
+
+group :development, :test do
+	gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+	gem 'faker', '0.3.1'
+end
+
+group :development do
+	gem 'rspec-rails', '2.6.1'
+	gem 'annotate'
+	gem 'faker', '0.3.1'
+end
+
+group :test do
+	gem 'rspec-rails', '2.6.1'
+	gem 'webrat', '0.7.1'
+	gem 'factory_girl_rails'
+
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karma::Application.config.secret_token = 'a9cae7c41ad1d25d0b36b9fcb2b212e41e788fda9d44222bfd814ca088d5c9141d71ef451216f37c076cc3dbd1594f499c38b567985e5f4be6b5c78b9f6c4cb9'
